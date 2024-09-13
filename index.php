ab42d6e009867126460cddb895b1d3df0fe1b8f3
Sebastian Pelcastre <sebastian03841@gmail.com>
2024-09-13 17:54:52 -0600

    push de prueba a prod

--- index.php
+++ index.php
@@ -5,9 +5,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Diagrama de Gantt</title>
+    <script src="frappe-gantt.min.js"></script>
 </head>
 
 <body>
+
+    <svg id="gantt"></svg>
 
 </body>
 
